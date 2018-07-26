a915ab74632050c59111544b1cd4b1a6239002ce
Marlon Secundo <marlon_secundo@outlook.com>
2018-07-26 11:43:54 -0300

    Implemented Navigation

--- App.js
+++ App.js
@@ -1,58 +1,24 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+import { Home, Settings } from './screens';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const RootStack = createStackNavigator(
+  {
+    HomeScreen: Home,
+    SettingsScreen: Settings
+  },
+  {
+    initialRouteName: 'HomeScreen',
+    mode: 'modal',
+    headerMode: 'none',
+  }
+);
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  render() {
+    return <RootStack />;
+  }
+
+}
---
+++ screens/Settings/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export class Settings extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Settings Screen </Text>
+      </View>
+    )
+  }
+}
+
+export default Settings;
